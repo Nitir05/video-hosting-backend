802a4177944a4d46f28d97f5174abfe77bb24290
Nitin Rahangdale <nitin.rahangdale@neuraflash.com>
2024-01-18 14:44:31 +0530

    Util Code Added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (err) => {
+            console.error(`Express server error: ${err}`);
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("mongo DB Connection Failed !!!", err);
+    });
